692309be925bd5efa3dd957b6f2a16647f91869d
seifHesham23 <121696089+seifHesham23@users.noreply.github.com>
2023-10-08 01:49:49 +0200

    Update medicines.js
    modified medicines model

--- server/models/medicines.js
+++ server/models/medicines.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
-const { nanoid } = require('nanoid');
+
 
 const medicineSchema = new mongoose.Schema({
-  identity: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   name: {
     type: String,
     required: true,
@@ -45,28 +40,8 @@ const medicineSchema = new mongoose.Schema({
   }
 });
 
-// Pre-save hook to generate a unique identity for each new medicine object
-medicineSchema.pre('save', async function (next) {
-  try {
-    // Generate a new identity using nanoid
-    const newIdentity = nanoid();
 
-    // Check if the identity is already in use
-    const existingMedicine = await this.constructor.findOne({ identity: newIdentity });
 
-    if (existingMedicine) {
-      // If the identity is already in use, generate a new one recursively
-      return this.save();
-    }
-
-    // If the identity is unique, assign it to the medicine object
-    this.identity = newIdentity;
-
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
 
 const MedicineModel = mongoose.model('Medicine', medicineSchema);
 
