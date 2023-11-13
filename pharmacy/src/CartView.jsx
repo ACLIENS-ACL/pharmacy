e842d9f166c9c47a4d88d12dc3173b8288100763
khawagaa <126677542+khawagaa@users.noreply.github.com>
2023-11-13 18:41:39 +0200

    Handled Frontend of Cart and Checkout

---
+++ pharmacy/src/CartView.jsx
@@ -0,0 +1,195 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function CartView() {
+  const [cartItems, setCartItems] = useState([]);
+  const navigate = useNavigate();
+  const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  const removeItem = (itemId) => {
+    const updatedCart = cartItems.filter((item) => item.name !== itemId);
+    setCartItems(updatedCart);
+    axios
+      .post('http://localhost:3001/remove-item', { cart: updatedCart })
+      .then((response) => {})
+      .catch((error) => {
+        console.error('Error removing item:', error);
+      });
+  };
+  
+  useEffect(() => {
+    // Fetch admin data from the server
+    axios.get(`http://localhost:3001/patient`)
+      .then((response) => {
+        const responseData = response.data;
+        if (responseData.type !== "patient" || responseData.in !== true) {
+          navigate('/login')
+        }
+      })
+  }, []);
+
+  const updateQuantity = (itemId, newQuantity) => {
+    if (newQuantity <= 0) {
+      removeItem(itemId);
+    } else {
+      const updatedCart = cartItems.map((item) => {
+        if (item.name === itemId) {
+          return { ...item, quantity: newQuantity };
+        }
+        return item;
+      });
+      setCartItems(updatedCart);
+
+      axios
+        .post('http://localhost:3001/update-quantity', { cart: updatedCart })
+        .then((response) => {})
+        .catch((error) => {
+          console.error('Error updating quantity:', error);
+        });
+    }
+  };
+
+  const handleConfirmOrder = () => {
+    navigate('/checkOut', { state: { cart: cartItems } });
+  };
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/view-cart')
+      .then((response) => {
+        const responseData = response.data;
+        if (responseData.userType === 'patient' && responseData.sessi === true) {
+          setCartItems(responseData.patientRequests.cart);
+        } else {
+          navigate('/login');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching cart data:', error);
+      });
+  }, [navigate]);
+
+  const handleLogout = () => {
+    axios.get(`http://localhost:3001/logout`).then((response) => {
+      const responseData = response.data;
+      if (responseData.type === '') {
+        navigate('/login');
+      }
+    });
+  };
+
+  const styles = {
+    container: {
+      margin: '20px auto',
+      width: '80%',
+      textAlign: 'center',
+    },
+    header: {
+      fontSize: '30px', // Increased font size
+      marginBottom: '20px',
+    },
+    cartItem: {
+      border: '1px solid #ddd',
+      borderRadius: '5px',
+      padding: '20px', // Increased padding for better spacing
+      marginBottom: '15px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#fff',
+      fontSize: '18px', // Increased font size for items
+    },
+    quantityButtons: {
+      display: 'flex',
+      marginTop: '10px',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    quantityButton: {
+      cursor: 'pointer',
+      padding: '5px 5px', // Increased padding
+      fontSize: '15px', // Increased font size
+      borderRadius: '3px',
+      border: '1px solid #007BFF',
+      backgroundColor: '#007BFF',
+      color: '#fff',
+      marginRight:'2px'
+    },
+    removeButton: {
+      marginTop: '10px',
+      padding: '8px 16px',
+      backgroundColor: '#dc3545',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+    total: {
+      marginTop: '20px',
+      fontWeight: 'bold',
+      fontSize: '20px', // Increased font size
+    },
+    confirmOrderButton: {
+      marginTop: '20px',
+      padding: '12px 24px',
+      backgroundColor: '#28a745',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    },
+    logoutButton: {
+      position: 'absolute',
+      top: '20px',
+      right: '20px',
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.logoutButton}>
+        <button onClick={handleLogout} className="btn btn-danger">
+          Logout
+        </button>
+      </div>
+      <h1 style={styles.header}>Shopping Cart</h1>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <div key={item.name} style={styles.cartItem}>
+            <div>{item.name}</div>
+            <div>Price: ${item.price}</div>
+            <div>
+              Quantity: {item.quantity}
+              <div style={styles.quantityButtons}>
+                <button
+                  style={styles.quantityButton}
+                  onClick={() => updateQuantity(item.name, item.quantity + 1)}
+                >
+                  +
+                </button>
+                <button
+                  style={styles.quantityButton}
+                  onClick={() => updateQuantity(item.name, item.quantity - 1)}
+                  disabled={item.quantity <= 1}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <button style={styles.removeButton} onClick={() => removeItem(item.name)}>
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+      <p style={styles.total}>Total: ${total}</p>
+      <button style={styles.confirmOrderButton} onClick={handleConfirmOrder}>
+        Confirm Order
+      </button>
+    </div>
+  );
+}
+
+export default CartView;
---
+++ pharmacy/src/checkout.jsx
@@ -0,0 +1,289 @@
+// CheckOut.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+
+const containerStyle = {
+  maxWidth: '600px',
+  margin: '0 auto',
+  padding: '20px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+};
+
+const selectStyle = {
+  width: '100%',
+  padding: '10px',
+  marginBottom: '10px',
+  border: '1px solid #ccc',
+  borderRadius: '3px',
+};
+
+const buttonStyle = {
+  padding: '10px',
+  background: 'blue',
+  color: 'white',
+  border: 'none',
+  borderRadius: '3px',
+  cursor: 'pointer',
+};
+
+const tableStyle = {
+  width: '100%',
+  borderCollapse: 'collapse',
+  marginBottom: '10px',
+};
+
+const thStyle = {
+  backgroundColor: 'lightgray',
+  padding: '8px',
+  textAlign: 'left',
+};
+
+const tdStyle = {
+  border: '1px solid #ddd',
+  padding: '8px',
+};
+
+const addressHeaderStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+const stripePromise = loadStripe('pk_test_51O88P5HzoCWXbTYqT8xDcGsLRVepjiG6k4KqILsc5mIxkTraEfRqAP9N6Vr3yRdQHDcuB8R4M5C754kjshcm1JtM0051zRZXTh');
+
+const CheckoutForm = ({ onPaymentSuccess }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    const { token, error } = await stripe.createToken(card);
+
+    if (error) {
+      console.error(error);
+    } else {
+      // Send the token to your server to charge the user.
+      // You can handle the server-side logic for payment here.
+
+      // Example: Call your API to handle the payment
+      // const response = await axios.post('your-payment-endpoint', { token });
+      // if (response.data.success) {
+      //   onPaymentSuccess();
+      // }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+function CheckOut() {
+  const [deliveryAddresses, setDeliveryAddresses] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState('');
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+  const [walletBalance, setWalletBalance] = useState(0);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const cartData = location.state?.cart || [];
+  const total = cartData.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
+  useEffect(() => {
+    // Fetch the wallet balance from the server
+    axios.get('http://localhost:3001/wallet-balance') // Adjust the endpoint as needed
+      .then((response) => {
+        setWalletBalance(response.data.balance);
+      })
+      .catch((error) => {
+        console.error('Error fetching wallet balance:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    // Fetch delivery addresses from the server
+    axios
+      .get('http://localhost:3001/delivery-addresses')
+      .then((response) => {
+        const responseData = response.data;
+        if (responseData.userType === 'patient' && responseData.sessi === true) {
+          setDeliveryAddresses(responseData.patientRequests[0].deliveryAddresses);
+        } else {
+          navigate('/login');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching delivery addresses:', error);
+      });
+  }, [navigate]);
+
+  const handleAddressChange = (event) => {
+    setSelectedAddress(event.target.value);
+  };
+
+  const placeOrder = () => {
+    if (!selectedAddress || !selectedPaymentMethod) {
+      alert('Please select a delivery address and payment method.');
+      return;
+    }
+  
+    const orderData = {
+      cart: cartData,
+      deliveryAddress: selectedAddress,
+      paymentMethod: selectedPaymentMethod,
+    };
+  
+    if (selectedPaymentMethod === 'wallet') {
+      console.log(walletBalance)
+      console.log(total)
+      if (walletBalance < total) {
+        alert('Insufficient funds in the wallet. Please choose another payment method.');
+        return;
+      }
+    }
+  
+    // Send a request to the server to place the order
+    axios
+      .post('http://localhost:3001/place-order', orderData)
+      .then((response) => {
+        if (selectedPaymentMethod === 'wallet') {
+          const newWalletBalance = walletBalance - total;
+      
+          // Update the wallet balance on the server
+          axios.post('http://localhost:3001/update-wallet-balance', { balance: newWalletBalance })
+            .then(() => {
+              setWalletBalance(newWalletBalance);
+            })
+            .catch((error) => {
+              console.error('Error updating wallet balance:', error);
+            });
+        }
+        axios
+          .get('http://localhost:3001/update-medicine-quantities', {})
+        navigate('/patient'); // Redirect to a success page
+      })
+      .catch((error) => {
+        console.error('Error placing the order:', error);
+        alert('Failed to place the order. Please try again.');
+      });
+  };
+
+  const handleAddAddress = () => {
+    navigate('/addAddress');
+  };
+
+  const handlePaymentMethodChange = (event) => {
+    setSelectedPaymentMethod(event.target.value);
+  };
+
+  const handleLogout = () => {
+    // Perform any necessary logout actions (e.g., clearing session or tokens).
+    // After logging out, navigate to the login page.
+    // Fetch admin data from the server
+    axios.get(`http://localhost:3001/logout`)
+      .then((response) => {
+        const responseData = response.data;
+        if (responseData.type === "") {
+          navigate('/login');
+        }
+      });
+  };
+
+  useEffect(() => {
+    // Fetch admin data from the server
+    axios.get(`http://localhost:3001/patient`)
+      .then((response) => {
+        const responseData = response.data;
+        if (responseData.type !== "patient" || responseData.in !== true) {
+          navigate('/login');
+        }
+      });
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div className="d-flex justify-content-end mb-2">
+        <button onClick={handleLogout} className="btn btn-danger">Logout</button>
+      </div>
+      <h2>Cart Summary</h2>
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th style={thStyle}>Item</th>
+            <th style={thStyle}>Price</th>
+            <th style={thStyle}>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartData.map((item) => (
+            <tr key={item.name}>
+              <td style={tdStyle}>{item.name}</td>
+              <td style={tdStyle}>${item.price}</td>
+              <td style={tdStyle}>{item.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <p>Total: ${total}</p>
+
+      <div style={addressHeaderStyle}>
+        <h2>Delivery Addresses</h2>
+        <button onClick={handleAddAddress} style={buttonStyle}>
+          Add New Address
+        </button>
+      </div>
+
+      <select style={selectStyle} value={selectedAddress} onChange={handleAddressChange}>
+        <option value="">Select an address</option>
+        {deliveryAddresses.map((address) => (
+          <option key={address} value={address}>
+            {address}
+          </option>
+        ))}
+      </select>
+      <p>Selected Address: {selectedAddress}</p>
+
+      <h2>Payment Method</h2>
+      <select
+        style={selectStyle}
+        value={selectedPaymentMethod}
+        onChange={handlePaymentMethodChange}
+      >
+        <option value="">Select a payment method</option>
+        <option value="wallet">Wallet</option>
+        <option value="credit_card">Credit Card (Stripe)</option>
+        <option value="cash_on_delivery">Cash on Delivery</option>
+      </select>
+
+      {selectedPaymentMethod === 'credit_card' && (
+        <Elements stripe={stripePromise}>
+          <CheckoutForm onPaymentSuccess={() => navigate('/patient')} />
+        </Elements>
+      )}
+
+      <button onClick={placeOrder} style={buttonStyle}>
+        Place Order
+      </button>
+    </div>
+  );
+}
+
+export default CheckOut;
